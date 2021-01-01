b5899cad9a6187079078233f1d6a1095d9734e4a
wutao <497863986@qq.com>
2021-01-01 15:59:18 +0800

    command test done

--- renames/command/command_test.go
+++ renames/command/command_test.go
@@ -7,17 +7,20 @@ import (
 func Test_Execute(t *testing.T) {
 	// commands for testing
 	var inputRequired = map[string][]string{
-		"input": []string{"-i", "the input file"},
+		"input": []string{"-i", "./"},
 	}
 	var commands = map[string][]string{
-		"input":     []string{},
-		"output":    []string{"-o", "output"},
-		"reverse":   []string{"-r"},
-		"pick":      []string{"-p", "1", "-p", "4"},
-		"name":      []string{"name"},
-		"time":      []string{"time"},
-		"size":      []string{"size"},
-		"extension": []string{"extension"},
+		"input":   []string{},
+		"output":  []string{"-o", "./"},
+		"begin":   []string{"-b"},
+		"pick":    []string{"-p", "1", "-p", "4"},
+		"reverse": []string{"-r"},
+		"list":    []string{"-l", "1"},
+		"add":     []string{"add", "-c", "added char"},
+		"delete":  []string{"delete", "-n", "5"},
+		"rename":  []string{"rename", "-c", "renamed char"},
+		"listcom": []string{"list", "-e"},
+		"count":   []string{"count", "-c", "counted char"},
 	}
 
 	for k, v := range commands {
@@ -25,6 +28,9 @@ func Test_Execute(t *testing.T) {
 		args := append(inputRequired["input"], v...)
 		cmd.SetArgs(args)
 		config := cmd.Execute()
-		t.Log(k, "\t->\t", *config)
+		t.Log(k, " -> ", config.Files, config.OutputDir,
+			config.Begin, config.Pick, config.Reverse,
+			config.ListFile, config.Add, config.Delete,
+			config.Rename, config.List, config.Count)
 	}
 }
